123fac4428d19cb9a15695453f81a75609df40cd
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-03-16 16:18:40 +0000

    Project Files Uploaded

---
+++ gif/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static'), filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
